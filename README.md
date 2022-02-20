1d297ee3060d5bcf0501306ffb3a041266593705
Freewind <nowindlee@gmail.com>
2022-02-20 19:38:38 +0800

    typescript-react-tailwind-table-demo

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-TypeScript React tailwind Demo
+TypeScript React tailwind Table Demo
 =================================
 
-在React中以及.pcss中都可以使用tailwind来定制样式
-
-在Webstorm中可以很好的支持className的提示和补全
+使用tailwind做一个table
 
 注意：.pcss中语法如果出错（比如少了`;`），将会报source map的错误，有点误导
 
--- src/Hello.tsx
+++ src/Hello.tsx
@@ -5,7 +5,39 @@ type Props = {};
 
 export const Hello: FC<Props> = ({}) => {
   return <div className={'Hello'}>
-    <h1>Hello React</h1>
-    <button className='rounded'>Hello</button>
+    <table className="table-auto">
+      <thead>
+      <tr className={'hover:bg-gray-200'}>
+        <th className="px-4 py-2">Title</th>
+        <th className="px-4 py-2">Author</th>
+        <th className="px-4 py-2">Views</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr className={'hover:bg-gray-200'}>
+        <td className="border"><input className={'w-full h-10 px-4 py-2 hover:bg-gray-200 focus:bg-white'} defaultValue={'Intro to CSS'}/></td>
+        <td className="border px-4 py-2">Adam</td>
+        <td className="border px-4 py-2">858</td>
+      </tr>
+      <tr className="bg-gray-100 hover:bg-gray-200">
+        <td className="border px-4 py-2">A Long and Winding Tour of the
+        </td>
+        <td className="border px-4 py-2">Adam</td>
+        <td className="border px-4 py-2">112</td>
+      </tr>
+      <tr className={'hover:bg-gray-200'}>
+        <td className="border px-4 py-2">Intro to JavaScript</td>
+        <td className="border px-4 py-2">Chris</td>
+        <td className="border px-4 py-2">1,280</td>
+      </tr>
+      </tbody>
+      <tfoot>
+      <tr className={'hover:bg-gray-200'}>
+        <td className="border px-4 py-2">foot</td>
+        <td className="border px-4 py-2">foot</td>
+        <td className="border px-4 py-2">foot</td>
+      </tr>
+      </tfoot>
+    </table>
   </div>;
 }
